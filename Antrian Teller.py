7c2ff0a711aaf3362c560a5e713bf780c20739ce
radenalam <radenalamsanjaya@gmail.com>
2021-11-24 21:25:01 +0700

    button belum work

--- Antrian Teller.py
+++ Antrian Teller.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter as tk
 
 antrian_tl = []
 antrian_cs = []
@@ -7,24 +7,12 @@ nomor_teller = 1
 nomor_cs = 1
 nomor_cb = 1
 
-main_window = tkinter.Tk()
-main_window.title("BANK Yogyakarta")
-main_window.geometry("600x300")
-
-label = tkinter.Label(main_window, text="Selamat Datang di Bank")
-label.pack()
-
-
-
-main_window.mainloop()
-
 def cetak_antrian_teller():
 	nomor_teller_tl = 'T' + str(nomor_teller)
 	antrian_tl.append(nomor_teller)
 	nomor_teller +=1
-	print(" ")
-	print("Nomor antrian Teller :" + nomor_teller_tl )
-	print(" ")
+	antriansekarang.set(nomor_teller_tl)
+	label2.config(font=('Helvetica', 15, "bold"), fg="red")
 
 def cetak_antrian_cs():
 	nomor_teller_cs = 'CS' + str(nomor_cs)
@@ -41,6 +29,35 @@ def cetak_buku():
 	print(" ")
 	print("Nomor antrian Teller :" + nomor_teller_cb )
 	print(" ")
+
+main_window = tk.Tk()
+main_window.title("BANK Yogyakarta")
+main_window.geometry("600x300")
+
+label = tk.Label(main_window, text="Selamat Datang di Bank")
+label.pack()
+
+
+teller = tk.Button(main_window, text="Teller", command=cetak_antrian_teller)
+teller.pack()
+teller.place(x = 20, y = 50)
+
+cs = tk.Button(main_window, text="Custumer Service")
+cs.pack()
+cs.place(x = 150, y = 50)
+
+buku = tk.Button(main_window, text="Cetak Buku")
+buku.pack()
+buku.place(x = 300, y = 50)
+
+antriansekarang = tk.StringVar()
+antriansekarang.set("0")
+label2 = tk.Label(main_window, font=('Helvetica', 12, "bold"), textvariable=antriansekarang)
+label2.pack()
+
+main_window.mainloop()
+
+
 """"
 while True:
 		print("SELAMAT DATANG DI BANK")
