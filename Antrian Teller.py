d36bf087e8cfca01e12ce991734ba83717d021b0
radenalam <radenalamsanjaya@gmail.com>
2021-11-24 20:45:21 +0700

    update

--- Antrian Teller.py
+++ Antrian Teller.py
@@ -8,11 +8,39 @@ nomor_cs = 1
 nomor_cb = 1
 
 main_window = tkinter.Tk()
-label = tkinter.Label(main_window, text="Selamat Datang di Bank")
+main_window.title("BANK Yogyakarta")
+main_window.geometry("600x300")
 
+label = tkinter.Label(main_window, text="Selamat Datang di Bank")
 label.pack()
+
+
+
 main_window.mainloop()
 
+def cetak_antrian_teller():
+	nomor_teller_tl = 'T' + str(nomor_teller)
+	antrian_tl.append(nomor_teller)
+	nomor_teller +=1
+	print(" ")
+	print("Nomor antrian Teller :" + nomor_teller_tl )
+	print(" ")
+
+def cetak_antrian_cs():
+	nomor_teller_cs = 'CS' + str(nomor_cs)
+	antrian_cs.append(nomor_cs)
+	nomor_cs +=1
+	print(" ")
+	print("Nomor antrian Teller :" + nomor_teller_cs )
+	print(" ")
+
+def cetak_buku():
+	nomor_teller_cb = 'CB' + str(nomor_cb)
+	antrian_cb.append(nomor_cb)
+	nomor_cb +=1
+	print(" ")
+	print("Nomor antrian Teller :" + nomor_teller_cb )
+	print(" ")
 """"
 while True:
 		print("SELAMAT DATANG DI BANK")
